3e6eaf4f66cecbff3e65fabc22ea162f59a705c5
Aniol <aniollidon@gmail.com>
2024-11-20 20:03:09 +0100

    Millores als botons de screens

--- palamSRV/src/v1/admin/js/screens.js
+++ palamSRV/src/v1/admin/js/screens.js
@@ -56,6 +56,32 @@ function drawGridItem(alumne, maquina) {
 
     const itemButtons = document.createElement("div");
     itemButtons.classList.add("grid-item-buttons");
+
+
+    // Botó d'apagar
+    const buttonOff = document.createElement("button");
+    buttonOff.classList.add("btn", "btn-sm", "btn-danger");
+    buttonOff.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
+                              <path d="M7.5 1v7h1V1z"/>
+                              <path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812"/>
+                            </svg>`;
+    buttonOff.onclick = () => {
+        socket.emit('sendCommandToAlumne', {alumne: alumne, command: "apaga-tot"});
+    }
+    itemButtons.appendChild(buttonOff);
+
+    // Botó actualitzar vista
+    const buttonRefresh = document.createElement("button");
+    buttonRefresh.classList.add("btn", "btn-sm", "btn-dark");
+    buttonRefresh.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41m-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9"/>
+      <path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5 5 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"/>
+    </svg>`;
+    buttonRefresh.onclick = () => {
+        iframe.src = `http://${maquina.ip}:6080/vnc_iframe.html?password=fpb123&reconnect&name=${alumne}`;
+    }
+    itemButtons.appendChild(buttonRefresh);
+
     // Botó de pantalla completa
     const buttonFull = document.createElement("button");
     buttonFull.classList.add("btn", "btn-sm", "btn-dark");
@@ -67,34 +93,27 @@ function drawGridItem(alumne, maquina) {
     }
     itemButtons.appendChild(buttonFull);
 
-    // Botó visió incognit
-    const buttonIncognito = document.createElement("button");
-    buttonIncognito.classList.add("btn", "btn-sm", "btn-dark");
-    buttonIncognito.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5"/>
+    // Llista extra de botons
+    const moreButtons = document.createElement("div");
+    moreButtons.classList.add("dropdown", "d-inline", "extra-item-buttons", "dropend");
+    const buttonMore = document.createElement("button");
+    buttonMore.classList.add("btn", "btn-sm", "btn-dark");
+    buttonMore.setAttribute("type", "button");
+    buttonMore.setAttribute("id", "mesBotons_" + alumne);
+    buttonMore.setAttribute("data-bs-toggle", "dropdown");
+    buttonMore.setAttribute("aria-expanded", "false");
+    buttonMore.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
     </svg>`;
-    buttonIncognito.onclick = () => {
-        // Open link in new tab
-        window.open(`http://${maquina.ip}:6080/vnc.html?reconnect&viewonly=true&name=${alumne}`, '_blank');
-    }
-    itemButtons.appendChild(buttonIncognito);
-    
-    
-    // Botó actualitzar script
-    const buttonScript = document.createElement("button");
-    buttonScript.classList.add("btn", "btn-sm", "btn-dark");
-    buttonScript.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-upload" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>
-  <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708z"/>
-</svg>`;
-    buttonScript.onclick = () => {
-        socket.emit('sendCommandToAlumne', {alumne: alumne, command: "actualitza"});
-    }
-    itemButtons.appendChild(buttonScript);
+
+    const dropdownMenu = document.createElement("ul");
+    dropdownMenu.classList.add("dropdown-menu");
+    dropdownMenu.setAttribute("aria-labelledby", "mesBotons_" + alumne);
+
     // Botó de congelar
+    const dropdownItem1 = document.createElement("li");
     const buttonFreeze = document.createElement("button");
     buttonFreeze.classList.add("btn", "btn-sm", "btn-primary");
-    buttonFreeze.style.display = "none"; // TODO: funcionalitat amagada
 
     function setButtonFreezeText(estat) {
         buttonFreeze.setAttribute("data-estat", estat);
@@ -124,19 +143,55 @@ function drawGridItem(alumne, maquina) {
 
         }
     }
-    itemButtons.appendChild(buttonFreeze);
+    dropdownItem1.appendChild(buttonFreeze);
+    dropdownMenu.appendChild(dropdownItem1);
 
-    // Botó d'apagar
-    const buttonOff = document.createElement("button");
-    buttonOff.classList.add("btn", "btn-sm", "btn-danger");
-    buttonOff.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
-                              <path d="M7.5 1v7h1V1z"/>
-                              <path d="M3 8.812a5 5 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812"/>
-                            </svg>`;
-    buttonOff.onclick = () => {
-        socket.emit('sendCommandToAlumne', {alumne: alumne, command: "apaga-tot"});
+    // Botó actualitzar script
+    const dropdownItem2 = document.createElement("li");
+    const buttonScript = document.createElement("button");
+    buttonScript.classList.add("btn", "btn-sm", "btn-dark");
+    buttonScript.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-upload" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383"/>
+      <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708z"/>
+    </svg>`;
+    buttonScript.onclick = () => {
+        socket.emit('sendCommandToAlumne', {alumne: alumne, command: "actualitza"});
     }
-    itemButtons.appendChild(buttonOff);
+    dropdownItem2.appendChild(buttonScript);
+    dropdownMenu.appendChild(dropdownItem2);
+
+    // Botó visió incognit
+    const dropdownItem3 = document.createElement("li");
+    const buttonIncognito = document.createElement("button");
+    buttonIncognito.classList.add("btn", "btn-sm", "btn-dark");
+    buttonIncognito.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-incognito" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="m4.736 1.968-.892 3.269-.014.058C2.113 5.568 1 6.006 1 6.5 1 7.328 4.134 8 8 8s7-.672 7-1.5c0-.494-1.113-.932-2.83-1.205l-.014-.058-.892-3.27c-.146-.533-.698-.849-1.239-.734C9.411 1.363 8.62 1.5 8 1.5s-1.411-.136-2.025-.267c-.541-.115-1.093.2-1.239.735m.015 3.867a.25.25 0 0 1 .274-.224c.9.092 1.91.143 2.975.143a30 30 0 0 0 2.975-.143.25.25 0 0 1 .05.498c-.918.093-1.944.145-3.025.145s-2.107-.052-3.025-.145a.25.25 0 0 1-.224-.274M3.5 10h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5m-1.5.5q.001-.264.085-.5H2a.5.5 0 0 1 0-1h3.5a1.5 1.5 0 0 1 1.488 1.312 3.5 3.5 0 0 1 2.024 0A1.5 1.5 0 0 1 10.5 9H14a.5.5 0 0 1 0 1h-.085q.084.236.085.5v1a2.5 2.5 0 0 1-5 0v-.14l-.21-.07a2.5 2.5 0 0 0-1.58 0l-.21.07v.14a2.5 2.5 0 0 1-5 0zm8.5-.5h2a.5.5 0 0 1 .5.5v1a1.5 1.5 0 0 1-3 0v-1a.5.5 0 0 1 .5-.5"/>
+    </svg>`;
+    buttonIncognito.onclick = () => {
+        // Open link in new tab
+        window.open(`http://${maquina.ip}:6080/vnc.html?reconnect&viewonly=true&name=${alumne}`, '_blank');
+    }
+
+    dropdownItem3.appendChild(buttonIncognito);
+    dropdownMenu.appendChild(dropdownItem3);
+
+    // Botó copiar IP
+    const dropdownItem4 = document.createElement("li");
+    const buttonCopyIP = document.createElement("button");
+    buttonCopyIP.classList.add("btn", "btn-sm", "btn-dark");
+    buttonCopyIP.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard2" viewBox="0 0 16 16">
+      <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1z"/>
+      <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5"/>
+    </svg>`;
+    buttonCopyIP.onclick = () => {
+        navigator.clipboard.writeText(maquina.ip);
+    }
+    dropdownItem4.appendChild(buttonCopyIP);
+    dropdownMenu.appendChild(dropdownItem4);
+
+    moreButtons.appendChild(buttonMore);
+    moreButtons.appendChild(dropdownMenu);
+    itemButtons.appendChild(moreButtons);
     gridItemContentHeader.appendChild(itemButtons);
     gridItem.appendChild(gridItemContentHeader);
 
@@ -149,10 +204,9 @@ function drawGridItem(alumne, maquina) {
         gridItem.classList.add("offline");
         // Desactiva els botons
         buttonFull.disabled = true;
-        buttonIncognito.disabled = true;
-        buttonScript.disabled = true;
-        buttonFreeze.disabled = true;
-        buttonOff.disabled = true
+        buttonMore.disabled = true;
+        buttonOff.disabled = true;
+        buttonRefresh.disabled = true;
     }
     else {
         iframe.setAttribute("src",
@@ -160,10 +214,9 @@ function drawGridItem(alumne, maquina) {
         gridItem.classList.add("online");
         // Reactiva els botons
         buttonFull.disabled = false;
-        buttonIncognito.disabled = false;
-        buttonScript.disabled = false;
-        buttonFreeze.disabled = false;
+        buttonMore.disabled = false;
         buttonOff.disabled = false;
+        buttonRefresh.disabled = false;
     }
 
     iframe.setAttribute("width", "400px");
